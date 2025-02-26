4c4df2c543cf10552aee564f54d36a0c350992f0
Roy Lopez <150288003+oy0139@users.noreply.github.com>
2025-02-26 16:07:12 -0500

    Add base structure for calculator

--- script.js
+++ script.js
@@ -3,5 +3,19 @@ const operators = ['+', '-', '*', ' / '];
 const add = (a,b) => a + b;
 const subtract = (a, b) => a - b;
 const multiply = (a, b) => a * b;
-const divide = (a, b) => a * b;
+const divide = (a, b) => {
+    if (b ==0) {
+        return "Error: you cant do that"
+    }
+    else {
+        return a / b
+    }
+}
+let
+
+
+let number1 = 
+let operator =
+let number2 = 
+let displayValue = 
 
